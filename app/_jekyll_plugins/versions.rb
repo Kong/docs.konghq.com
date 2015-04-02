de88473f636eeee5f5387c1c918807eafe573a92
Thibault Charbonnier <thibaultcha@me.com>
2015-04-03 01:03:31 +0200

    feat: versioning jekyll plugin for our docs

---
+++ app/_jekyll_plugins/versions.rb
@@ -0,0 +1,20 @@
+module Jekyll
+
+  class Test < Jekyll::Generator
+    def generate(site)
+      # Add a `version` property to every versioned page
+      site.pages.each do |page|
+        parts = Pathname(page.path).each_filename.to_a
+        if site.config["documentation"] == parts[0]
+          puts(parts[1])
+          page.data["kong_version"] = parts[1]
+        end
+      end
+
+      # Retrieve the latest version and put it in `site.data.kong_latest`
+      latest = site.data["versions"].last
+      site.data["kong_latest"] = latest
+    end
+  end
+
+end
