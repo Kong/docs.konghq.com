513d7b5f5c394759c94c8ac739c9654dcd1349a6
Thibault Charbonnier <thibaultcha@me.com>
2015-04-03 02:02:01 +0200

    style: cleaning up versioning plugin

--- app/_jekyll_plugins/versions.rb
+++ app/_jekyll_plugins/versions.rb
@@ -6,13 +6,12 @@ module Jekyll
       site.pages.each do |page|
         parts = Pathname(page.path).each_filename.to_a
         if site.config["documentation"] == parts[0]
-          puts(page.name)
           page.data["kong_version"] = parts[1]
         end
       end
 
       # Retrieve the latest version and put it in `site.data.kong_latest`
-      latest = site.data["versions"].last
+      latest = site.data["kong_versions"].last
       site.data["kong_latest"] = latest
     end
   end
