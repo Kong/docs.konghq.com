3645fbc1cf33aaa9d7f14b99d711eafc712e3227
Thibault Charbonnier <thibaultcha@me.com>
2015-04-08 01:11:00 +0200

    Simplify gulpfile and jekyll config
    - Some Jekyll config properties were jsut the default values
    - simpler gulpfile
    - standard is now a company policy on how we write JavaScript, so
      converted the gulpfile to it

--- app/docs/0.1.1beta-2/index.md
+++ app/docs/0.1.1beta-2/index.md
@@ -1,5 +1,4 @@
 ---
-layout: docs
 title: Documentation
 permalink: /docs/
 ---
--- gulpfile.js
+++ gulpfile.js
@@ -1,46 +1,41 @@
-'use strict';
+'use strict'
 
-var gulp = require('gulp');
-var path = require('path');
-var glob = require('glob');
-var ghPages = require('gh-pages');
-var sequence = require('run-sequence');
+var gulp = require('gulp')
+var ghPages = require('gh-pages')
+var sequence = require('run-sequence')
 
 // load gulp plugins
-var $ = require('gulp-load-plugins')();
-var connect = $.connect;
+var $ = require('gulp-load-plugins')()
+var connect = $.connect
 
 // Build variables (default to local config)
 var jekyllConfigs = {
   PROD: 'config/jekyll.yml',
   LOCAL: 'config/jekyll.local.yml'
-};
+}
 
-var jekyllConfig = jekyllConfigs.LOCAL;
+var jekyllConfig = jekyllConfigs.LOCAL
 
 // Sources
 var sources = {
-  styles: 'app/_assets/stylesheets/index.less',
+  content: 'app/**/*.{markdown,md,html,txt}',
+  styles: 'app/_assets/stylesheets/*.{less,css}',
   js: 'app/_assets/javascripts/**/*.js',
   images: 'app/_assets/images/**/*'
-};
+}
 
 gulp.task('styles', function () {
+  var glob = require('glob')
   // TODO: add LESS linting
 
   // thibaultcha:
   // 1. gulp-less has plugins (minifier and prefixer) we can run in $.less(plugins: [clean, prefix])
   // but they throw errors if we use them. Let's use gulp-autoprefixer and gulp-minify-css.
-  //
   // 2. If we want to use uncss, we need to minify after it runs, otherwise it unminifies the css
   // another reason not to use gulp-less plugins.
-  //
   // 3. the source maps still don't work if used along with gulp-autoprefixer
-  //
   // 4. uncss and gulpminify-css seem to behave fine and is not having an impact on sourcemaps
-  //
-  // 5. fuck this shit.
-  return gulp.src(sources.styles)
+  return gulp.src('app/_assets/stylesheets/index.less')
     .pipe($.plumber())
     .pipe($.sourcemaps.init())
     .pipe($.less())
@@ -51,8 +46,8 @@ gulp.task('styles', function () {
     .pipe($.rename('styles.css'))
     .pipe(gulp.dest('dist/assets/'))
     .pipe($.size())
-    .pipe(connect.reload());
-});
+    .pipe(connect.reload())
+})
 
 gulp.task('javascripts', function () {
   return gulp.src(sources.js)
@@ -65,26 +60,26 @@ gulp.task('javascripts', function () {
     .pipe($.sourcemaps.write('maps'))
     .pipe(gulp.dest('dist/assets'))
     .pipe($.size())
-    .pipe(connect.reload());
-});
+    .pipe(connect.reload())
+})
 
 gulp.task('images', function () {
-  // Just copy images
   return gulp.src(sources.images)
     .pipe($.plumber())
     .pipe(gulp.dest('dist/assets/images'))
-    .pipe($.size());
-});
+    .pipe($.size())
+    .pipe(connect.reload())
+})
 
 gulp.task('jekyll', function (next) {
-  var command = 'bundle exec jekyll build --config ' + jekyllConfig + ' --destination dist';
+  var command = 'bundle exec jekyll build --config ' + jekyllConfig + ' --destination dist'
 
   require('child_process').exec(command, function (err, stdout, stderr) {
-    console.log(stdout);
-    console.error(stderr);
-    next(err);
-  });
-});
+    console.log(stdout)
+    console.error(stderr)
+    next(err)
+  })
+})
 
 gulp.task('html', ['jekyll'], function () {
   return gulp.src('dist/**/*.html')
@@ -102,19 +97,22 @@ gulp.task('html', ['jekyll'], function () {
     }))
     .pipe(gulp.dest('dist'))
     .pipe($.size())
-    .pipe(connect.reload());
-});
+    .pipe(connect.reload())
+})
 
 gulp.task('clean', function (cb) {
-  ghPages.clean();
-  require('del')(['dist', '.gh-pages'], cb);
-});
+  ghPages.clean()
+  require('del')(['dist', '.gh-pages'], cb)
+})
 
-gulp.task('reload', function () {
-  return gulp.src('dist')
-    .pipe($.plumber())
-    .pipe(connect.reload());
-});
+gulp.task('build', ['javascripts', 'images'], function () {
+  sequence('html', 'styles')
+})
+
+gulp.task('build:prod', function () {
+  jekyllConfig = jekyllConfigs.PROD
+  sequence('build')
+})
 
 gulp.task('connect', function () {
   connect.server({
@@ -122,39 +120,29 @@ gulp.task('connect', function () {
     root: 'dist',
     livereload: true,
     fallback: 'dist/404.html'
-  });
-});
-
-gulp.task('watch', function () {
-  gulp.watch(['app/**/*.html', 'app/**/*.txt', 'app/**/*.xml', 'app/**/*.md'], function () {
-    sequence('jekyll', 'reload');
-  });
-  gulp.watch('app/_assets/javascripts/**/*.js', ['javascripts']);
-  gulp.watch('app/_assets/images/**/*', ['images']);
-  gulp.watch('app/_assets/stylesheets/**/*.{less,css}', ['styles']);
-});
-
-gulp.task('build', ['javascripts', 'images'], function () {
-  sequence('html', 'styles');
-});
-
-gulp.task('build:prod', function () {
-  jekyllConfig = jekyllConfigs.PROD;
-  sequence('build');
-});
-
-gulp.task('default', ['clean'], function () {
-  sequence('build', 'connect', 'watch');
-});
+  })
+})
 
 gulp.task('gh-pages', function (next) {
+  var path = require('path')
   var config = {
     message: 'Update ' + new Date().toISOString()
-  };
+  }
 
-  ghPages.publish(path.join(__dirname, 'dist'), config, next);
-});
+  ghPages.publish(path.join(__dirname, 'dist'), config, next)
+})
 
 gulp.task('deploy:prod', function () {
-  sequence('build:prod', 'gh-pages');
-});
+  sequence('build:prod', 'gh-pages')
+})
+
+gulp.task('watch', function () {
+  gulp.watch(sources.content, ['build'])
+  gulp.watch(sources.styles, ['styles'])
+  gulp.watch(sources.images, ['images'])
+  gulp.watch(sources.js, ['javascripts'])
+})
+
+gulp.task('default', ['clean'], function () {
+  sequence('build', 'connect', 'watch')
+})
