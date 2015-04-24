0f9612ffd3065a05dda16bd59acf7288a00dd84e
Vazha <rainum.ua@gmail.com>
2015-04-24 15:10:44 +0300

    homepage text fixes

--- app/_assets/javascripts/app.js
+++ app/_assets/javascripts/app.js
@@ -4,6 +4,14 @@ $(function () {
     $navbar.slideToggle(150);
   });
 
+  $('.scroll-to').on('click', function (e) {
+    e.preventDefault();
+
+    $('html, body').animate({
+      scrollTop: $($(this).attr('href')).offset().top
+    }, 700);
+  });
+
   // Page section on contribute page
 
   $('.toggle-page-section').on('click', function (e) {
