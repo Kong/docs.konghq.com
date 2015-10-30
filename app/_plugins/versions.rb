894944cc3342e79840f318dc9de68bf5f12d8b54
Thibault Charbonnier <thibaultcha@me.com>
2015-10-30 13:12:53 -0700

    fix(versions plugin) also alias nested index.html files

--- app/_plugins/versions.rb
+++ app/_plugins/versions.rb
@@ -9,22 +9,26 @@ module Jekyll
       site.data["kong_latest"] = latest
 
       # Add a `version` property to every versioned page
+      # Also create aliases under /latest/ for all x.x.x doc pages
       site.pages.each do |page|
         parts = Pathname(page.path).each_filename.to_a
+
+        # Only apply those rules to documentation pages
         if parts[0] == site.config["documentation"]
           page.data["kong_version"] = parts[1]
+
           # Put navigation items for current version in page.nav_items
           page.data["nav_items"] = site.data['docs_nav_' + parts[1].gsub(/\./, '')]
 
           # Alias latest docs folder /docs/x.x.x to /docs/latest
           if parts[1] == latest["release"]
             page.data["alias"] = "/" + page.path.sub(parts[1], "latest").sub(/\..*$/, "")
-            # the /docs/ page
             if parts[2] == "index.md"
+              # the /docs/ page
               page.data["permalink"] = "/docs/"
               page.data["alias"] = ["/#{site.config["documentation"]}/latest", "/#{site.config["documentation"]}/#{latest["release"]}/index.html"]
-            # all other nested index.md pages
-            elsif parts.last == "index.md"
+            elsif /index\.(md|html)/.match(parts.last)
+              # all other nested index pages
               # /docs/latest/plugin-development/index/index.html -> /docs/latest/plugin-development/index.html
               page.data["alias"] = page.data["alias"].sub(/index/, "")
             end
