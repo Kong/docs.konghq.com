549212c729afe09d5a33fb7c54579060e574463b
Enrico Sacchetti <esacchetti@gmail.com>
2016-06-12 22:37:20 -0400

    refactor(cloud): improve Typeform logic
    Remove the typeform-clickzone div whether the user clicks on 'request demo' or the typeform itself.

--- app/_assets/javascripts/app.js
+++ app/_assets/javascripts/app.js
@@ -165,6 +165,7 @@ $(function () {
   // Cloud page demo request Typeform
 
   var expandTypeform = function () {
+    $('#typeform-clickzone').addClass('clicked')
     $('#enterprise').slideUp(600, function () {
       var subtractHeight = $('.site-header').height() + $('.footer').height()
       $('#typeform')
@@ -179,7 +180,6 @@ $(function () {
 
   $('#typeform-clickzone').one('click', function () {
     expandTypeform()
-    $(this).addClass('clicked')
   })
 
   // Enterprise page demo request form
