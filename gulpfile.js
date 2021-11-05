46fc35316b2cfbcb103b0c62dec9f82008f0e3b9
lena-larionova <54370747+lena-larionova@users.noreply.github.com>
2021-11-05 09:57:04 -0700

    [DOCU-1963] Handling access to older split versions and adding announcement (#31)
    * adjust dropdown in sidebar to show older/newer versions
    
    * adjustments to kong version metadata to remove 2.6.x split versions from nav
    
    * enable promo banner to add doc announcement

--- gulpfile.js
+++ gulpfile.js
@@ -33,7 +33,7 @@ var sources = {
     paths.assets + "javascripts/feedback-widget.js",
     // uncomment the path to promo-banner.js when adding a new promo banner
     // also uncomment the promo banner sections in app/_assets/stylesheets/header.less and /app/_includes/nav-v2.html -->
-    // paths.assets + "javascripts/promo-banner.js",
+    paths.assets + "javascripts/promo-banner.js",
     paths.assets + "javascripts/copy-code-snippet-support.js"
   ],
   images: paths.assets + "images/**/*",
