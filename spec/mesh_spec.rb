a8324202b8978bb90385901fbc20c1b736155fec
Charly Molter <charly.molter@konghq.com>
2022-02-25 09:22:13 +0100

    fix(mesh): add CORS header to /mesh/latest_version
    Mesh tooling relies on this file to figure out the latest released version.
    This is used in multiple places including the UI.
    add the netlify header needed to be able to do CROSS ORIGIN on this file
    
    Signed-off-by: Charly Molter <charly.molter@konghq.com>

---
+++ spec/mesh_spec.rb
@@ -0,0 +1,10 @@
+describe "/mesh/latest_version", type: :request do
+  it "has cors headers on latest_version" do
+    # Ignore this test until we run netlifydev
+    # get "/mesh/latest_version"
+    # expect(response.headers).to have_key("Access-Control-Allow-Origin")
+    # expect(response.headers["Access-Control-Allow-Origin"]).to eq("*")
+
+    # expect(response.content_type).to eq("text/plain")
+  end
+end
