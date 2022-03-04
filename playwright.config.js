613e82fda5a46e76b358c8b0dcc7e6b60bb808bc
Michael Heap <m@michaelheap.com>
2022-03-04 11:46:09 +0000

    Install Playwright

---
+++ playwright.config.js
@@ -0,0 +1,22 @@
+// playwright.config.js
+// @ts-check
+const { devices } = require("@playwright/test");
+
+/** @type {import('@playwright/test').PlaywrightTestConfig} */
+const config = {
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  testDir: "tests",
+  use: {
+    trace: "on-first-retry",
+    baseURL: "http://localhost:8888/",
+  },
+  projects: [
+    {
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
+    },
+  ],
+};
+
+module.exports = config;
