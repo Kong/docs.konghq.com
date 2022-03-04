fbc671c60c9041f6370884afae70ee819b741ca7
Charly Molter <charly.molter@konghq.com>
2022-03-04 19:55:48 +0100

    remove spec changes
    Signed-off-by: Charly Molter <charly.molter@konghq.com>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,7 +6,6 @@ require "rack/test"
 require "pry"
 require "capybara/apparition"
 RSpec.configure do |config|
-
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
