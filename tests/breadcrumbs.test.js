6fc5ae1473241610bea3b111ef9f46bfbbc6d1ee
Michael Heap <m@michaelheap.com>
2022-03-04 12:58:19 +0000

    Migrate tests from rspec to playwright

---
+++ tests/breadcrumbs.test.js
@@ -0,0 +1,54 @@
+const { test, expect } = require("@playwright/test");
+
+test.describe("Gateway", () => {
+  test("renders nested breadcrumbs correctly (EE)", async ({ page }) => {
+    await page.goto("/enterprise/2.5.x/plugin-development/custom-entities/");
+    await expect(page.locator(".breadcrumb-item:nth-of-type(1)")).toHaveText(
+      "Kong Konnect Platform"
+    );
+    await expect(page.locator(".breadcrumb-item:nth-of-type(2)")).toHaveText(
+      "Kong Gateway"
+    );
+    await expect(page.locator(".breadcrumb-item:nth-of-type(3)")).toHaveText(
+      "Plugin development"
+    );
+  });
+
+  test("renders nested breadcrumbs correctly (OSS)", async ({ page }) => {
+    await page.goto("/gateway-oss/2.5.x/plugin-development/custom-entities/");
+    await expect(page.locator(".breadcrumb-item:nth-of-type(1)")).toHaveText(
+      "Open Source"
+    );
+    await expect(page.locator(".breadcrumb-item:nth-of-type(2)")).toHaveText(
+      "Kong Gateway (OSS)"
+    );
+    await expect(page.locator(".breadcrumb-item:nth-of-type(3)")).toHaveText(
+      "Plugin development"
+    );
+  });
+
+  test("renders nested breadcrumbs correctly (Single Sourced)", async ({
+    page,
+  }) => {
+    await page.goto("/gateway/");
+    await expect(page.locator(".breadcrumb-item:nth-of-type(1)")).toHaveText(
+      "Kong Konnect Platform"
+    );
+    await expect(page.locator(".breadcrumb-item:nth-of-type(2)")).toHaveText(
+      "Kong Gateway"
+    );
+  });
+});
+
+test.describe("decK", () => {
+  test("rrenders the index page breadcrumbs correctly", async ({ page }) => {
+    await page.goto("/deck/");
+
+    await expect(page.locator(".breadcrumb-item:nth-of-type(1)")).toHaveText(
+      "Open Source"
+    );
+    await expect(page.locator(".breadcrumb-item:nth-of-type(2)")).toHaveText(
+      "decK"
+    );
+  });
+});
