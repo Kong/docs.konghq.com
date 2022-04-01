fea36e5d93ffe57f88952f4e0e6829d8b3ca4816
Michael Heap <m@michaelheap.com>
2022-04-01 18:09:58 +0100

    Add robots:noindex for versioned URLs

--- app/_plugins/generators/canonical_url_generator.rb
+++ app/_plugins/generators/canonical_url_generator.rb
@@ -54,6 +54,8 @@ module CanonicalUrl
           has_match = allPages[u]
           if has_match
             page.data['canonical_url'] = has_match['url']
+            # We don't want to index any page that has a canonical URL
+            page.data['robots'] = 'noindex'
           end
         end
       end
