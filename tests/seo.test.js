70d3c7d31ae0a39f8f9d6d75d239cf7b3616440d
Michael Heap <m@michaelheap.com>
2022-04-08 12:41:25 +0100

    Updated tests

--- tests/seo.test.js
+++ tests/seo.test.js
@@ -34,19 +34,43 @@ test.describe("noindex links", () => {
   [
     {
       title: "contains a noindex tag if it's not the latest URL",
-      src: "/enterprise/2.3.x/admin-api/",
-      expected: 1
+      src: "/gateway/2.7.x/install-and-run/",
     },
     {
-      title: "latest page does not contain a noindex meta tag",
+      title: "contains a noindex tag if it's a gateway-oss page",
+      src: "/gateway-oss/2.5.x/",
+    },
+    {
+      title: "contains a noindex tag if it's an enterprise page",
+      src: "/enterprise/2.5.x/",
+    },
+    {
+      title: "contains a noindex tag if it's a getting-started-guide page",
+      src: "/getting-started-guide/2.5.x/prepare/",
+    },
+  ].forEach((t) => {
+    test(t.title, async ({ page }) => {
+      await page.goto(t.src);
+      const link = page.locator(
+        "meta[name='robots'][content='follow,noindex']"
+      );
+      await expect(link).toHaveCount(1);
+    });
+  });
+});
+
+test.describe("index links", () => {
+  [
+    {
+      title: "latest page",
       src: "/gateway/latest/install-and-run/docker/",
       expected: 0,
     },
   ].forEach((t) => {
     test(t.title, async ({ page }) => {
       await page.goto(t.src);
-      const link = page.locator("meta[name='robots'][content='noindex']");
-      await expect(link).toHaveCount(t.expected);
+      const link = page.locator("meta[name='robots'][content='follow,index']");
+      await expect(link).toHaveCount(1);
     });
   });
 });
