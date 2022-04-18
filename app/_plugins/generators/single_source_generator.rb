e100462c83c673e192de3f7805a599ad5f4e26e9
Michael Heap <m@michaelheap.com>
2022-04-18 22:04:28 +0100

    Add `version` to single sourcing (#3853)
    * Add page.release and page.version
    
    * Update single sourcing documentation
    
    * Generate or GenerateD - choose Generate

--- app/_plugins/generators/single_source_generator.rb
+++ app/_plugins/generators/single_source_generator.rb
@@ -3,21 +3,33 @@
 module SingleSource
   class Generator < Jekyll::Generator
     priority :highest
-    def generate(site)
+    def generate(site) # rubocop:disable Metrics/AbcSize
+      # Load versions data file
+      @kong_versions = SafeYAML.load(File.read('app/_data/kong_versions.yml'))
+
+      # Generate pages
       Dir.glob('app/_data/docs_nav_*.yml').each do |f|
         data = SafeYAML.load(File.read(f))
         next unless data.is_a?(Hash) && data['generate']
 
         # Assume that the whole file should be treated as generated
         assume_generated = data['assume_generated'].nil? ? true : data['assume_generated']
-        create_pages(data['items'], site, data['product'], data['version'], assume_generated)
+        version = version_for_release(data['product'], data['release'])
+        create_pages(data['items'], site, data['product'], data['release'], version, assume_generated)
       end
     end
 
-    def create_pages(data, site, product, version, assume_generated) # rubocop:disable Metrics/PerceivedComplexity, Metrics/CyclomaticComplexity, Metrics/AbcSize
+    def version_for_release(product, release)
+      version = @kong_versions.detect do |v|
+        v['edition'] == product && v['release'] == release
+      end
+      version['version']
+    end
+
+    def create_pages(data, site, product, release, version, assume_generated) # rubocop:disable Metrics/PerceivedComplexity, Metrics/CyclomaticComplexity, Metrics/AbcSize, Metrics/ParameterLists
       data.each do |v, _k|
         # Enable generation of specific files as required
-        next unless v['generated'] || assume_generated
+        next unless v['generate'] || assume_generated
 
         # Handle when it's the root page.
         # We always want to generate this, even if
@@ -30,17 +42,17 @@ module SingleSource
           # Is it an in-page link? If so, skip it
           next if v['url']&.include?('/#')
 
-          site.pages << SingleSourcePage.new(site, v['src'], v['url'], product, version)
+          site.pages << SingleSourcePage.new(site, v['src'], v['url'], product, release, version)
         end
 
         # If there are any children, generate those too
-        create_pages(v['items'], site, product, version, assume_generated) if v['items']
+        create_pages(v['items'], site, product, release, version, assume_generated) if v['items']
       end
     end
   end
 
   class SingleSourcePage < Jekyll::Page
-    def initialize(site, src, dest, product, version) # rubocop:disable Lint/MissingSuper, Metrics/CyclomaticComplexity, Metrics/MethodLength, Metrics/AbcSize
+    def initialize(site, src, dest, product, release, version) # rubocop:disable Lint/MissingSuper, Metrics/CyclomaticComplexity, Metrics/MethodLength, Metrics/AbcSize, Metrics/ParameterLists
       # Configure variables that Jekyll depends on
       @site = site
 
@@ -51,7 +63,7 @@ module SingleSource
       src ||= dest
 
       # Remove trailing slashes if they exist
-      src.chomp!('/')
+      src = src.chomp('/')
 
       # Set self.ext and self.basename by extracting information from the page filename
       process('index.md')
@@ -61,7 +73,7 @@ module SingleSource
       output_path = '' if src == 'index'
 
       # This is the directory that we're going to write the output file to
-      @dir = "#{product}/#{version}/#{output_path}"
+      @dir = "#{product}/#{release}/#{output_path}"
 
       # If the src file doesn't start with a /, assume it's within the product folder
       # Otherwise, it's an absolute src path and we should start from /src
@@ -80,6 +92,10 @@ module SingleSource
 
       # Set the "Edit on GitHub" link url
       @data['edit_link'] = file
+
+      # Set the current release and concrete version
+      @data['release'] = release
+      @data['version'] = version
 
       # Set the layout if it's not already provided
       @data['layout'] = 'docs-v2' unless data['layout']
