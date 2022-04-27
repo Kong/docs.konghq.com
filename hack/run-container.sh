687b212154982c6fe753165e0794acb4fbfbe8e6
usrbinkat <usrbinkat@braincraft.io>
2022-04-27 15:30:37 -0700

    working containerized local docs tooling

--- hack/run-container.sh
+++ hack/run-container.sh
@@ -1,2 +1,4 @@
 #!/bin/bash -x
-docker run -d --rm -p 3000:3000 -v $(pwd):/site --name docs ghcr.io/kong/docs:latest
+docker rm --force docs 2>&1 1>/dev/null
+docker run -d --rm --pull=never -p 3000:3000 -v $(pwd):/site --name docs ghcr.io/kong/docs:latest
+docker exec -it docs bash
