b50a0a796f8637b84418aa49527f006773b573e8
Michael Heap <m@michaelheap.com>
2022-05-27 21:16:11 +0100

    Always make generate:false skip generation

--- app/_plugins/generators/single_source_generator.rb
+++ app/_plugins/generators/single_source_generator.rb
@@ -31,9 +31,8 @@ module SingleSource
         # Enable generation of specific files as required
         next unless v['generate'] || assume_generated
 
-        # If the whole file is generated, we might want to exclude a specific URL
-        # by setting generate: false
-        next if assume_generated && v['generate'] === false
+        # We might want to exclude a specific URL by setting generate: false
+        next if v['generate'] === false
 
         # Handle when it's the root page.
         # We always want to generate this, even if
