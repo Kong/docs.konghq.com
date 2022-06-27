4928c59a28e3208a5a248141233fe30f7b993c68
Michael Heap <m@michaelheap.com>
2022-06-27 11:22:15 +0100

    Fix GitHub edit link for single sourced pages

---
+++ tests/edit_link.test.js
@@ -0,0 +1,37 @@
+const { test, expect } = require("@playwright/test");
+
+test.describe("Edit this page link", () => {
+  [
+    {
+      title: "/app/ page",
+      src: "/gateway/2.8.x/install-and-run/docker/",
+      expected:
+        "https://github.com/Kong/docs.konghq.com/edit/main/app/gateway/2.8.x/install-and-run/docker.md",
+    },
+
+    {
+      title: "Single Sourced",
+      src: "/deck/1.12.x/",
+      expected:
+        "https://github.com/Kong/docs.konghq.com/edit/main/src/deck/index.md",
+    },
+    {
+      title: "/app/ page /latest/",
+      src: "/gateway/latest/",
+      expected:
+        "https://github.com/Kong/docs.konghq.com/edit/main/app/gateway/2.8.x/index.md",
+    },
+    {
+      title: "Single Sourced /latest/",
+      src: "/deck/latest/",
+      expected:
+        "https://github.com/Kong/docs.konghq.com/edit/main/src/deck/index.md",
+    },
+  ].forEach((t) => {
+    test(t.title, async ({ page }) => {
+      await page.goto(t.src);
+      const s = await page.locator(".github-links a").first();
+      await expect(await s.getAttribute("href")).toEqual(t.expected);
+    });
+  });
+});
