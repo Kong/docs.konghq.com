e7bdd235e2ea60893e99271a2026cb5cf8ebed59
Michael Heap <m@michaelheap.com>
2022-08-24 20:16:33 +0100

    Add titleize gem + filter

---
+++ app/_plugins/filters/titleize_filter.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'active_support/core_ext/string/inflections' # If not using Rails
+
+ActiveSupport::Inflector.inflections do |inflect|
+  inflect.acronym 'API'
+  inflect.acronym 'mTLS'
+end
+
+module Jekyll
+  module TitleizeFilter
+    def titleize(input)
+      input.titleize
+    end
+  end
+end
+
+Liquid::Template.register_filter(Jekyll::TitleizeFilter)
