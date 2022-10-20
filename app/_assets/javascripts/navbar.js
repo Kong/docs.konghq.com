7084b3f99f5a7a85bb9692ad3a805c720058d644
Michael Heap <m@michaelheap.com>
2022-10-20 08:19:25 +0100

    Reformat app/{_assets,_hub,_data} with Prettier

--- app/_assets/javascripts/navbar.js
+++ app/_assets/javascripts/navbar.js
@@ -1,53 +1,58 @@
 // from nav-v2.html
 
-function getSearchPlaceholder () {
-  var w = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+function getSearchPlaceholder() {
+  var w =
+    window.innerWidth ||
+    document.documentElement.clientWidth ||
+    document.body.clientWidth;
   if (w <= 800) {
-    document.getElementById('getkong-algolia-search-input').placeholder = 'Search...';
+    document.getElementById("getkong-algolia-search-input").placeholder =
+      "Search...";
   } else {
-    document.getElementById('getkong-algolia-search-input').placeholder = 'Search the docs...';
+    document.getElementById("getkong-algolia-search-input").placeholder =
+      "Search the docs...";
   }
 }
 
 getSearchPlaceholder();
-window.addEventListener('resize', getSearchPlaceholder);
+window.addEventListener("resize", getSearchPlaceholder);
 
-function handleSearchClicked () {
-    $('.navbar-v2').toggleClass('search-opened')
-    $('.navbar-v2').toggleClass('menu-opened', false)
-    $('#getkong-algolia-search-input').focus()
+function handleSearchClicked() {
+  $(".navbar-v2").toggleClass("search-opened");
+  $(".navbar-v2").toggleClass("menu-opened", false);
+  $("#getkong-algolia-search-input").focus();
 }
 
-function toggleButtonClicked () {
-    if (!$('.navbar-v2').hasClass('search-opened')) {
-        $('.navbar-v2').toggleClass('menu-opened')
-    } else {
-        $('.navbar-v2').toggleClass('search-opened', false)
-    }
+function toggleButtonClicked() {
+  if (!$(".navbar-v2").hasClass("search-opened")) {
+    $(".navbar-v2").toggleClass("menu-opened");
+  } else {
+    $(".navbar-v2").toggleClass("search-opened", false);
+  }
 }
 
-function toggleSubmenuVisible (element) {
-    $(element).toggleClass('submenu-title')
-    $(element.parentElement).toggleClass('submenu-opened')
+function toggleSubmenuVisible(element) {
+  $(element).toggleClass("submenu-title");
+  $(element.parentElement).toggleClass("submenu-opened");
 }
 
 // open Docs menu item upon enter and enable tabbing through menu
 jQuery(function () {
-  $("#docs-link").on("keypress keydown", function(e) {
-    if(e.keyCode == 13) {
+  $("#docs-link").on("keypress keydown", function (e) {
+    if (e.keyCode == 13) {
       e.preventDefault();
       $(".with-submenu").toggleClass("submenu-opened");
       $(".navbar-item-docs").setAttribute("aria-hidden", "false");
       return false;
     }
     // if user doesn't open Docs submenu, move focus to Support menu item
-    let submenu = $(".with-submenu")
-    if(!submenu.hasClass("submenu-opened")) {
+    let submenu = $(".with-submenu");
+    if (!submenu.hasClass("submenu-opened")) {
       $("#plugin-link").focus();
     }
-  })
+  });
   // close docs dropdown menu when tabbing on Support menu item
-  $("#plugin-link").on("focus", function(e) {
+  $("#plugin-link").on("focus", function (e) {
     $(".with-submenu").removeClass("submenu-opened");
   });
 });
