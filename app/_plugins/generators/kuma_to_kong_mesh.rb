669e2b2d2df0e97127c18cd078fd3412a42f641b
Michael Heap <m@michaelheap.com>
2022-11-02 16:47:40 +0000

    Add Kuma->Kong Mesh transforms

--- app/_plugins/generators/kuma_to_kong_mesh.rb
+++ app/_plugins/generators/kuma_to_kong_mesh.rb
@@ -5,9 +5,19 @@ module KumaToKongMesh
     priority :lowest
     def generate(site)
       site.pages.each do |page|
-        next unless page.data['title']
-
+        next unless page.data['path']&.include?("docs_nav_mesh")
         page.data['title'] = page.data['title'].gsub('Kuma', 'Kong Mesh')
+
+        # Replace kuma with kong-mesh in links
+        # Links can end with a " (html) or ) (markdown)
+        page.content = page.content.gsub(%r{/([^\s]*)kuma([^\s]*)([)"])}, '/\1kong-mesh\2\3')
+        page.content = page.content.gsub("kong-mesh.io", "kuma.io")
+
+        # Replace Kuma with Kong Mesh in prose
+        page.content = page.content.gsub("Kuma", "Kong Mesh")
+
+        # Replace the base url from Kuma
+        page.content = page.content.gsub(%r{/docs/{{ page.version }}}, '/mesh/{{ page.kong_version }}')
       end
     end
   end
