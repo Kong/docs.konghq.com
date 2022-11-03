6b7630fbeca530cb82dd666582b676f64435a6db
Michael Heap <m@michaelheap.com>
2022-11-03 14:07:19 +0000

    Fix Rubocop errors

--- app/_plugins/generators/kuma_to_kong_mesh.rb
+++ app/_plugins/generators/kuma_to_kong_mesh.rb
@@ -4,15 +4,16 @@
 module KumaToKongMesh
   class Generator < Jekyll::Generator
     priority :lowest
-    def generate(site)
+    def generate(site) # rubocop:disable Metrics/AbcSize
       site.pages.each do |page|
-        next unless page.data['path']&.include?("docs_nav_mesh")
+        next unless page.data['path']&.include?('docs_nav_mesh')
+
         page.data['title'] = page.data['title'].gsub('Kuma', 'Kong Mesh')
 
         # Replace kuma with kong-mesh in links
         # Links can end with a " (html) or ) (markdown)
         page.content = page.content.gsub(%r{/([^\s]*)kuma([^\s]*)([)"])}, '/\1kong-mesh\2\3')
-        page.content = page.content.gsub("kong-mesh.io", "kuma.io")
+        page.content = page.content.gsub('kong-mesh.io', 'kuma.io')
 
         # Replace the base url from Kuma
         page.content = page.content.gsub(%r{/docs/{{ page.version }}}, '/mesh/{{ page.kong_version }}')
