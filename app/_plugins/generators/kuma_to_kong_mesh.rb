d88fbff8b1498c375b89b93cc40f7482a8abf362
Michael Heap <m@michaelheap.com>
2022-11-04 12:04:37 +0000

    Fix install, enterprise and community links in Mesh

--- app/_plugins/generators/kuma_to_kong_mesh.rb
+++ app/_plugins/generators/kuma_to_kong_mesh.rb
@@ -11,9 +11,18 @@ module KumaToKongMesh
         page.data['title'] = page.data['title'].gsub('Kuma', 'Kong Mesh')
 
         # Replace kuma with kong-mesh in links
-        # Links can end with a " (html) or ) (markdown)
-        page.content = page.content.gsub(%r{([("]/.+*)kuma(?!(?:-cp|-dp|ctl))([^\s]*)([)"])}, '\1kong-mesh\2\3')
+        # Links can be wrapped with " (html) or ( and ) (markdown)
+        page.content = page.content.gsub(%r{([("]/.*)kuma(?!(?:-cp|-dp|ctl))([^\s]*)([)"])}, '\1kong-mesh\2\3')
         page.content = page.content.gsub('kong-mesh.io', 'kuma.io')
+
+        exact_link_transforms = {
+          '/install/?' => '/mesh/{{ page.kong_version }}/install/',
+          '/enterprise/?' => '/mesh/{{ page.kong_version }}/',
+          '/community/?' => 'https://konghq.com/community'
+        }
+        exact_link_transforms.each do |k,v|
+          page.content = page.content.gsub(%r{([("])#{k}([)"])}, "\\1#{v}\\2")
+        end
 
         # Replace the base url from Kuma
         page.content = page.content.gsub(%r{/docs/{{\s*page.version\s*}}}, '/mesh/{{ page.kong_version }}')
