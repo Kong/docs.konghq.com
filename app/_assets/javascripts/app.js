76f9bf73b70461d52ea85f2363451c0199335db3
lena-larionova <54370747+lena-larionova@users.noreply.github.com>
2022-11-10 13:58:13 -0800

    Clean up CTA form styling and move to stylesheet (#4753)
    * clean up cta form styling and move to stylesheet
    
    * move error styling to stylesheet
    
    * add max width to avoid overflow
    
    * set explicit border color to avoid illusion of smaller button
    
    * appease linter

--- app/_assets/javascripts/app.js
+++ app/_assets/javascripts/app.js
@@ -658,7 +658,7 @@ jQuery(function () {
   // Signup confirmation for nurture campaigns
   if (getParams.signup == "nurture") {
     $("#nurture-signup").html(
-      "Thanks for signing up! Check your emails to see the first step of your learning journey"
+      "Thanks for signing up! Check your emails to see the first step of your learning journey."
     );
   }
 
@@ -667,7 +667,7 @@ jQuery(function () {
       .removeClass("note")
       .addClass("warning")
       .prepend(
-        '<span style="color:red;font-weight:800;">There was an error adding your free training. Please try again</span>'
+        '<span class="cta-error">There was an error adding your free training. Please try again.</span>'
       );
   }
 
