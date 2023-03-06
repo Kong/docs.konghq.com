1fc370ce44512714504dec4e83acb559fee196a6
Michael Heap <m@michaelheap.com>
2023-03-06 12:27:04 +0000

    Ensure all external links have target=_blank and rel=noopener

---
+++ app/_plugins/hooks/external_links.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require 'nokogiri'
+
+module Jekyll
+  Jekyll::Hooks.register :pages, :post_render do |page|
+    # Only process pages. This prevents _redirects being output with a HTML doctype
+    next unless page.instance_of?(Jekyll::Page)
+
+    has_changes = false
+    doc = ::Nokogiri::HTML(page.output)
+    doc.css('a').each do |link|
+      href = link.attributes['href']&.value
+      next unless href # No href, skip
+      next unless href.start_with?('http://') || href.start_with?('https://') # Not an external link, skip
+
+      has_changes = true
+      link.set_attribute('target', '_blank')
+      link.set_attribute('rel', "noopener nofollow noreferrer #{link.attributes['rel']&.value}")
+    end
+
+    page.output = doc.to_html if has_changes
+  end
+end
