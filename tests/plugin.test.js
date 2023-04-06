e91bb741cec211ea2fb9e8a784aba031a5d1155a
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2023-04-06 12:24:50 +0200

    Partially fix smoke tests

--- tests/plugin.test.js
+++ tests/plugin.test.js
@@ -3,7 +3,7 @@
 describe("plugin shows the correct sample", () => {
   // Set a baseline on a common plugin (basic-auth)
   test("basic-auth (kong inc)", async () => {
-    const $ = await fetchPage("/hub/kong-inc/basic-auth/");
+    const $ = await fetchPage("/hub/kong-inc/basic-auth/reference/");
     await expect($(".navtab-content").first()).toContainText(
       '--data "name=basic-auth"'
     );
@@ -12,7 +12,7 @@ describe("plugin shows the correct sample", () => {
   // Make sure that a different plugin shows the correct name too
   // In case we cached basic-auth by coincidence
   test("cors (kong inc)", async () => {
-    const $ = await fetchPage("/hub/kong-inc/cors/");
+    const $ = await fetchPage("/hub/kong-inc/cors/reference/");
     await expect($(".navtab-content").first()).toContainText(
       '--data "name=cors"'
     );
@@ -20,7 +20,7 @@ describe("plugin shows the correct sample", () => {
 
   // Show a community plugin too
   test("salt (community)", async () => {
-    const $ = await fetchPage("/hub/salt/salt/");
+    const $ = await fetchPage("/hub/salt/salt/reference/");
     await expect($(".content").first()).toContainText(
       '--data "name=salt-agent"'
     );
