4cf3183bb1013fa23036b5eeb3dcdc5204e0158c
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2023-06-13 10:58:33 +0200

    Add VITE_JEKYLL_ENV variable so we can skip some js from executing in preview apps

--- app/_assets/javascripts/analytics.js
+++ app/_assets/javascripts/analytics.js
@@ -1,5 +1,5 @@
 import { AnalyticsBrowser } from '@segment/analytics-next'
 
-if (import.meta.env.PROD) {
+if (import.meta.env.VITE_JEKYLL_ENV === 'production') {
   window.analytics = AnalyticsBrowser.load({ writeKey: 'X7EZTdbdUKQ8M6x42SHHPWiEhjsfs1EQ' })
 }
