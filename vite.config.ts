50ac697174f0a8f9f949f4c01574f1e5d94ef306
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2023-06-20 14:48:49 +0200

    Import jquery from node_module instead of file and set up rollup so we can use jQuery and $

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import RubyPlugin from 'vite-plugin-ruby'
+import inject from '@rollup/plugin-inject';
 
 export default defineConfig({
   plugins: [
+    inject({
+      $: 'jquery',
+      jQuery: 'jquery',
+    }),
     RubyPlugin(),
   ]
 })
