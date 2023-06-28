d74f68f26ff955595c87eb554431cadc59fac2e2
lena.larionova <yelena.larionova@gmail.com>
2023-06-28 09:54:39 -0700

    update specs

--- spec/templates/plugin_with_versions_spec.rb
+++ spec/templates/plugin_with_versions_spec.rb
@@ -10,10 +10,6 @@ RSpec.describe 'Plugin page with multiple versions' do
   context 'versions with a specific file - _2.2.x' do
     let(:page_url) { '/hub/kong-inc/jwt-signer/2.5.x/' }
 
-    it 'renders the plugin\'s description' do
-      expect(html).to have_css('p', text: 'From _2.2.x.md: The Kong JWT Signer plugin makes it possible to verify and (re-)sign one or two tokens in a request.')
-    end
-
     it 'renders the content' do
       expect(html).to have_css('h2', text: '_2.2.x Description')
       expect(html).to have_css('p', text: 'content: Verify and (re-)sign one or two tokens in a request')
@@ -26,18 +22,10 @@ RSpec.describe 'Plugin page with multiple versions' do
       expect(html).to have_css('.badge.enterprise')
       expect(html).to have_css('.old-version-banner')
     end
-
-    it 'renders extra when present' do
-      expect(html).not_to have_css('strong', text: 'Configuration Notes:')
-    end
   end
 
   context 'versions using _index' do
     let(:page_url) { '/hub/kong-inc/jwt-signer/' }
-
-    it 'renders the plugin\'s description' do
-      expect(html).to have_css('p', text: "From _index.md:")
-    end
 
     it 'renders the content' do
       expect(html).to have_css('h2', text: 'Manage key signing')
@@ -52,11 +40,5 @@ RSpec.describe 'Plugin page with multiple versions' do
       expect(html).not_to have_css('.old-version-banner')
     end
 
-    it 'renders extra when present' do
-      expect(html).to have_css('strong', text: 'Configuration Notes:')
-      expect(html).to have_css('p', text: "Most of the parameters are optional, but you need to specify some options to actually\nmake the plugin work:")
-      expect(html).to have_css('p', text: 'For example, signature verification cannot be done without the plugin knowing about')
-      expect(html).to have_css('p', text: 'Also for introspection to work, you need to specify introspection endpoints')
-    end
   end
 end
