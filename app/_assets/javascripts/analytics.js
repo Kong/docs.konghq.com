94ae3aed43223f6217a25617ae81a9ece3bf55b1
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2023-07-06 16:09:09 +0200

    Change the way we track page views

--- app/_assets/javascripts/analytics.js
+++ app/_assets/javascripts/analytics.js
@@ -2,4 +2,7 @@ import { AnalyticsBrowser } from '@segment/analytics-next'
 
 if (import.meta.env.VITE_JEKYLL_ENV === 'production') {
   window.analytics = AnalyticsBrowser.load({ writeKey: 'X7EZTdbdUKQ8M6x42SHHPWiEhjsfs1EQ' })
+  window.analytics.ready(function() {
+    window.analytics.page();
+  });
 }
