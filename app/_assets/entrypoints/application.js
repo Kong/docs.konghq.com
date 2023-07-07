a4d8bb61caf9f1771b40460e914ee92433c39a5c
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2023-07-07 09:19:35 +0200

    Fix analytics
    There is a weird dependency between gtm and segment which requires
    segment to be loaded before gtm

--- app/_assets/entrypoints/application.js
+++ app/_assets/entrypoints/application.js
@@ -3,6 +3,7 @@
 // When using a plain API, perhaps it's better to generate an HTML entrypoint
 // and link to the scripts and stylesheets, and let Vite transform it.
 
+import "~/javascripts/analytics.js";
 import "~/javascripts/app.js";
 import "~/javascripts/compat-dropdown.js";
 import "~/javascripts/plugin-hub.js";
@@ -11,8 +12,8 @@ import "~/javascripts/editable-code-snippet.js";
 import "~/javascripts/navbar.js";
 import "~/javascripts/feedback-widget.js";
 import "~/javascripts/copy-code-snippet-support.js";
-import "~/javascripts/analytics.js";
 import "~/javascripts/content-header.js"
+
 
 // uncomment the path to promo-banner.js when adding a new promo banner
 // also uncomment the promo banner sections in app/_assets/stylesheets/header.less and /app/_includes/nav-v2.html -->
