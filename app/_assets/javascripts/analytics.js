6ea5ddd83af696aa373cdbe719fbe5c2f1e524c5
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2023-07-07 10:23:25 +0200

    Remove the need for VITE_JEKYLL_ENV

--- app/_assets/javascripts/analytics.js
+++ app/_assets/javascripts/analytics.js
@@ -1,9 +1,7 @@
 import { AnalyticsBrowser } from '@segment/analytics-next'
 
-if (import.meta.env.VITE_JEKYLL_ENV === 'production') {
-  window.analytics = new AnalyticsBrowser();
+window.analytics = new AnalyticsBrowser();
 
-  analytics
-    .load({ writeKey: 'X7EZTdbdUKQ8M6x42SHHPWiEhjsfs1EQ' })
-    .catch(function(error) { console.log(error) });
-}
+analytics
+  .load({ writeKey: 'X7EZTdbdUKQ8M6x42SHHPWiEhjsfs1EQ' })
+  .catch(function(error) { console.log(error) });
