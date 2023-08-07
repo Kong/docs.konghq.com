c524a4b2d84d0c4ef449aa6d4ae03780cec32b93
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2023-08-07 23:01:01 +0200

    Apply blue styles to docs

--- app/_assets/javascripts/content-header.js
+++ app/_assets/javascripts/content-header.js
@@ -1,14 +1,14 @@
 window.addEventListener('scroll',
   function () {
     var w = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-    if (w <= 800 && $(window).scrollTop() < 8) {
-      $('.content').toggleClass('scrolled', false);
-      $('.content-header').toggleClass('scrolled', false);
-      $('.content-version').show(250);
+    if (w <= 480 && $(window).scrollTop() < 8) {
+      $('.page-header-product-version').show(250);
+      $('.page-header-doc').toggleClass('scrolled', false);
+      $('.page--header-background-doc').toggleClass('scrolled', false);
     } else {
-      $('.content').toggleClass('scrolled', true);
-      $('.content-header').toggleClass('scrolled', true);
-      $('.content-version').hide(250);
+      $('.page-header-product-version').hide(250);
+      $('.page-header-doc').toggleClass('scrolled', true);
+      $('.page--header-background-doc').toggleClass('scrolled', true);
     }
   }
 );
