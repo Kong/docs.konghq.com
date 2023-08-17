b581dbd7b2bc31acc9683c7c5a22eae6a7b0dd01
lena.larionova <yelena.larionova@gmail.com>
2023-08-17 13:44:34 -0700

    add konnect api plugin examples

---
+++ app/_plugins/drops/plugins/examples/konnect.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+module Jekyll
+  module Drops
+    module Plugins
+      module Examples
+        class Konnect < Base
+          URLS = {
+            'consumer' => 'https://global.api.konghq.com/v2/{RUNTIME-GROUP-ID}/core-entities/consumers/{CONSUMER_NAME|ID}/plugins',
+            'consumer_group' => 'https://global.api.konghq.com/v2/{RUNTIME-GROUP-ID}/core-entities/consumer_groups/{CONSUMER_GROUP_NAME|ID}/plugins',
+            'global' => 'https://global.api.konghq.com/v2/{RUNTIME-GROUP-ID}/core-entities/plugins/',
+            'route' => 'https://global.api.konghq.com/v2/{RUNTIME-GROUP-ID}/core-entities/routes/{ROUTE_NAME|ID}/plugins',
+            'service' => 'https://global.api.konghq.com/v2/{RUNTIME-GROUP-ID}/core-entities/services/{SERVICE_NAME|ID}/plugins'
+          }.freeze
+
+          def params
+            @params ||= [
+              Fields::Konnect.new(key: 'name', values: plugin_name),
+              config_to_params
+            ].flatten
+          end
+
+          def url
+            @url ||= URLS.fetch(@type)
+          end
+
+          private
+
+          def config_to_params
+            @example.fetch('config', []).map do |field, values|
+              Fields::Konnect.make_for(
+                key: "config.#{field}",
+                values:,
+                options: field_config(field)
+              )
+            end
+          end
+
+          def field_config(_field)
+            # TODO: handle url-encode with overlays or metadata
+            {}
+            # @config.fetch('config', {}).detect { |f| f['name'] == field } || {}
+          end
+        end
+      end
+    end
+  end
+end
