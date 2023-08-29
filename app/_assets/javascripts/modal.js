c4c8b69651e75c0d85bd90782706c09fbd317f2d
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2023-08-29 17:05:01 +0200

    Add new konnect cta to docs (#5901)
    * Add new konnect cta to docs
    Add a new CTA for driving people to Konnect, checking whether customers dismiss it or follow it.
    
    This is only rendered in:
    * app/_src/gateway/index.md
    * app/_src/gateway/install/docker/index.md
    
    It sets a cookie (konnect-cta-card) to keep track of whether the overlay was dismissed or clicked, so we don't show it to customers again
    
    * Make vale happy
    
    * Add modal for konnect CTA card
    
    * Update `utm_content` param for the CTA card
    
    * Place the modal on the bottom right corner and update the CTA
    
    * Close modal when the overlay is clicked
    
    ---------
    
    Co-authored-by: Amy Goldsmith <59702069+acgoldsmith@users.noreply.github.com>

---
+++ app/_assets/javascripts/modal.js
@@ -0,0 +1,80 @@
+$(document).ready(function () {
+  const modal = document.querySelector("#modal");
+  const modalOpen = document.querySelector("#modal-open");
+  const modalClose = document.querySelector("#modal-close");
+  const modalOverlay = document.querySelector("#modal-overlay");
+  const page = document.querySelector(".page");
+
+  function toggleModal(force) {
+    modal.classList.toggle("closed", force);
+    modalOverlay.classList.toggle("closed", force);
+  }
+
+  function openModal() {
+    toggleModal(false);
+
+    modal.removeAttribute("aria-hidden");
+    page.setAttribute("aria-hiddden", true);
+    modal.querySelector(".button").focus();
+  }
+
+  function closeModal() {
+    toggleModal(true);
+
+    modal.setAttribute("aria-hiddden", true);
+    page.removeAttribute("aria-hidden");
+    document.querySelector(".skip-main").focus();
+  }
+
+  function focusNextElement() {
+    const focusableElements = Array.from(modal.querySelectorAll(".button"));
+    focusableElements.push(modalClose);
+    const numberOfFocusableElements = focusableElements.length;
+
+    let focusedElement = modal.querySelector(":focus");
+    let focusedElementIndex = focusableElements.indexOf(focusedElement);
+
+    if (focusedElementIndex === numberOfFocusableElements -1) {
+      focusableElements.at(0).focus();
+    } else {
+      focusableElements.at(focusedElementIndex + 1).focus();
+    }
+  }
+
+  if (modal.length !== 0) {
+    modal.addEventListener("keydown", function(e) {
+      switch (e.which) {
+        // Escape key
+        case 27:
+          e.preventDefault();
+          modalClose.click();
+          break;
+        // Tab key
+        case 9:
+          e.preventDefault();
+          focusNextElement();
+          break;
+      }
+    });
+
+    modalOpen.addEventListener("click", function(e) {
+      e.stopPropagation();
+
+      openModal();
+    });
+
+    modalOverlay.addEventListener("click", function(e) {
+      e.stopPropagation();
+
+      modalClose.click();
+    })
+
+    const buttons = modal.querySelectorAll(".modal-close, .button")
+
+    buttons.forEach(function(elem) {
+      elem.addEventListener("click", function() {
+        closeModal();
+      });
+    });
+  }
+});
