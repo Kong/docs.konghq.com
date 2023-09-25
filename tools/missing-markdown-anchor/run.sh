998de97dfa3d3cf2bb736400908c841b198ef648
Michael Heap <m@michaelheap.com>
2023-09-25 15:30:53 +0100

    Fix markdown broken link checking

--- tools/missing-markdown-anchor/run.sh
+++ tools/missing-markdown-anchor/run.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
-FILES=$(grep -lrE " \[.*\]\[.*\] " dist | grep -v vite)
-for i in $FILES; do 
-  ANCHOR=$(perl -ne 'print "$1\n" if / \[.*\]\[(.*)\]/' $i)
-  echo "$i: ${ANCHOR#dist/#}"
-done
+set -euxo pipefail
+
+FILES=$(grep -lrE " \[.*\]\[.*\] " dist | grep -v vite) || true
 
 if [[ ! -z "$FILES" ]]; then
+  for i in $FILES; do
+    ANCHOR=$(perl -ne 'print "$1\n" if / \[.*\]\[(.*)\]/' $i)
+    echo "$i: ${ANCHOR#dist/#}"
+  done
+
   echo ""
   echo "ERROR: Detected broken markdown references";
   echo ""
