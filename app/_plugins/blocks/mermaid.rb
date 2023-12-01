4d6ac5b5daee357945e98029d2e0d342456d98cf
Lena <lena.larionova@konghq.com>
2023-12-01 15:58:15 -0800

    test styling for mermaid

--- app/_plugins/blocks/mermaid.rb
+++ app/_plugins/blocks/mermaid.rb
@@ -6,7 +6,22 @@ module Jekyll
 
         "<script type='module'>"\
         "import mermaid from 'https://cdn.jsdelivr.net/npm/mermaid@10/dist/mermaid.esm.min.mjs';"\
-        "mermaid.initialize({ startOnLoad: true });"\
+        "mermaid.initialize({ 
+          startOnLoad: true,
+          theme: 'base',
+          themeVariables: {
+            'primaryColor': '#fff',
+            'primaryBorderColor': '#4a86e8',
+            'primaryTextColor': '#495c64',
+            'secondaryColor': '#fff',
+            'secondaryTextColor': '#5096f2',
+            'edgeLabelBackground': '#fff',
+            'clusterBkg': '#dcf2ffff',
+            'fontFamily': 'Roboto',
+            'fontSize': '15px',
+            'lineColor': '#99b0c0'
+            }
+          });"\
         "</script>"\
         "<pre class='mermaid'> #{text}  </pre>"
     
