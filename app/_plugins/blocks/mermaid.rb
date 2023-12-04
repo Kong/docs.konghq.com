1ebe856701fe53b14939659150402bcd3725121f
Angel <Guaris@users.noreply.github.com>
2023-12-04 15:39:15 -0500

    fix: Mermaid rubocop fail  (#6602)
    * rubocop refactor
    
    * test file

--- app/_plugins/blocks/mermaid.rb
+++ app/_plugins/blocks/mermaid.rb
@@ -4,25 +4,35 @@ module Jekyll
   class RenderMermaid < Liquid::Block
     def render(context)
       text = super
-      "<script type='module'>" \
-        "import mermaid from 'https://cdn.jsdelivr.net/npm/mermaid@10/dist/mermaid.esm.min.mjs';" \
-        "mermaid.initialize({
-          startOnLoad: true,
-          theme: 'base',
-          themeVariables: {
-            'primaryColor': '#fff',
-            'primaryBorderColor': '#4a86e8',
-            'primaryTextColor': '#495c64',
-            'secondaryColor': '#fff',
-            'secondaryTextColor': '#5096f2',
-            'edgeLabelBackground': '#fff',
-            'fontFamily': 'Roboto',
-            'fontSize': '15px',
-            'lineColor': '#99b0c0'
+      mermaid_script = generate_mermaid_script
+      mermaid_pre = "<pre class='mermaid'> #{text}  </pre>"
+
+      "#{mermaid_script}#{mermaid_pre}"
+    end
+
+    private
+
+    def generate_mermaid_script
+      <<~SCRIPT
+        <script type='module'>
+          import mermaid from 'https://cdn.jsdelivr.net/npm/mermaid@10/dist/mermaid.esm.min.mjs';
+          mermaid.initialize({
+            startOnLoad: true,
+            theme: 'base',
+            themeVariables: {
+              'primaryColor': '#fff',
+              'primaryBorderColor': '#4a86e8',
+              'primaryTextColor': '#495c64',
+              'secondaryColor': '#fff',
+              'secondaryTextColor': '#5096f2',
+              'edgeLabelBackground': '#fff',
+              'fontFamily': 'Roboto',
+              'fontSize': '15px',
+              'lineColor': '#99b0c0'
             }
-        });" \
-      '</script>' \
-      "<pre class='mermaid'> #{text}  </pre>"
+          });
+        </script>
+      SCRIPT
     end
   end
 end
