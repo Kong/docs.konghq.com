bfe5b4ef1ecd736b6d95a0d424f4f8d66f504d46
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2023-12-11 22:21:34 +0100

    Add plugin non single-sourced plugin pages to the sitemap and set their canonical_urls

--- spec/app/_plugins/generators/seo/index_entry_builder_spec.rb
+++ spec/app/_plugins/generators/seo/index_entry_builder_spec.rb
@@ -18,6 +18,12 @@ RSpec.describe SEO::IndexEntryBuilder do
       it { expect(subject).to be_a(SEO::IndexEntry::HubPage) }
     end
 
+    context 'non single-sourced plugin page' do
+      let(:page) { double('url' => '/hub/plugins/compatibility/', 'path' => '') }
+
+      it { expect(subject).to be_a(SEO::IndexEntry::HubHtmlPage) }
+    end
+
     context 'page belonging to a product that is versioned' do
       context 'global page' do
         let(:page_url) { '/gateway/changelog/' }
