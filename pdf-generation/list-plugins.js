8ff840081161d099e589cd97f3a69fdd6de9e7fb
Michael Heap <m@michaelheap.com>
2024-01-09 19:22:29 +0000

    Update PDF generation to use ESM modules

--- pdf-generation/list-plugins.js
+++ pdf-generation/list-plugins.js
@@ -1,17 +1,20 @@
-const fg = require('fast-glob')
+import fg from "fast-glob";
 
-module.exports = async function (plugin, version) {
-  plugin = plugin || '*'
-  version = version || '_index'
-  let files = await fg(`../app/_hub/kong-inc/${plugin}/${version}.md`)
+export default async function (plugin, version) {
+  plugin = plugin || "*";
+  version = version || "_index";
+  let files = await fg(`../app/_hub/kong-inc/${plugin}/${version}.md`);
   files = files.map((f) => {
-    return f.replace('../app/_hub/', '/hub/').replace(/\.md$/, '.html').replace("_index", 'index')
-  })
+    return f
+      .replace("../app/_hub/", "/hub/")
+      .replace(/\.md$/, ".html")
+      .replace("_index", "index");
+  });
 
   // Prefix with URL
   files = files.map((f) => {
-    return `http://localhost:3000${f}`
-  })
+    return `http://localhost:3000${f}`;
+  });
 
-  return files
+  return files;
 }
