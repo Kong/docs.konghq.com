4f9ba8fbbf9a4b95ea19c9d84e14bfd2b41945c8
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2024-01-16 10:20:19 +0100

    Fix build-plugin-pdf

--- pdf-generation/list-plugins.js
+++ pdf-generation/list-plugins.js
@@ -1,20 +1,33 @@
 import fg from "fast-glob";
 
 export default async function (plugin, version) {
-  plugin = plugin || "*";
-  version = version || "_index";
-  let files = await fg(`../app/_hub/kong-inc/${plugin}/${version}.md`);
-  files = files.map((f) => {
+  plugin = plugin;
+  version = version || '';
+
+  let urls = [
+    `/hub/kong-inc/${plugin}/${version}/`,
+    `/hub/kong-inc/${plugin}/${version}/changelog/`,
+    `/hub/kong-inc/${plugin}/${version}/configuration/`,
+    `/hub/kong-inc/${plugin}/${version}/how-to/basic-example/`,
+  ];
+
+  let urlsFromFiles = await fg(`../app/_hub/kong-inc/${plugin}/how-to/*`);
+  urlsFromFiles = urlsFromFiles.map((f) => {
     return f
-      .replace("../app/_hub/", "/hub/")
-      .replace(/\.md$/, ".html")
-      .replace("_index", "index");
+      .replace('../app/_hub/', '/hub/')
+      .replace('/how-to/', `/${version}/how-to/`)
+      .replace('_index', '')
+      .replace('.md', '');
+  });
+
+  urls = urls.concat(urlsFromFiles).map((u) => {
+    return u.replace('//', '/');
   });
 
   // Prefix with URL
-  files = files.map((f) => {
-    return `http://localhost:3000${f}`;
+  urls = urls.map((u) => {
+    return `http://localhost:8888${u}`;
   });
 
-  return files;
+  return urls;
 }
