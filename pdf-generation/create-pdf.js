8da3c8be5b550c11976dd65a92f502a37afc2d6b
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2024-01-18 15:53:38 +0100

    Gracefully handle 404s, don't include those in the pdf

--- pdf-generation/create-pdf.js
+++ pdf-generation/create-pdf.js
@@ -8,65 +8,67 @@ async function createPDF(name, urls) {
     defaultViewport: { "width": 595, "height": 842 } // A4 size in pixels
   });
   const page = await browser.newPage();
+  let result;
 
   console.log(`Generating ./pdfs/${name}.pdf`);
   for (const url of urls) {
     console.log(`Adding ${url}`);
 
-    await page.goto(url, {
+    result = await page.goto(url, {
       waitUntil: "domcontentloaded",
     });
 
-    // Reflect CSS used for screens instead of print
-    await page.emulateMediaType("screen");
+    if (result.status() !== 404) {
+      // Reflect CSS used for screens instead of print
+      await page.emulateMediaType("screen");
 
-    // Wait until all images and fonts have loaded
-    // via https://github.blog/2021-06-22-framework-building-open-graph-images/
-    await page.evaluate(async () => {
-      const selectors = Array.from(document.querySelectorAll("img"));
-      await Promise.all([
-        document.fonts.ready,
-        ...selectors.map((img) => {
-          // Image has already finished loading, let’s see if it worked
-          if (img.complete) {
-            // Image loaded and has presence
-            if (img.naturalHeight !== 0) return;
+      // Wait until all images and fonts have loaded
+      // via https://github.blog/2021-06-22-framework-building-open-graph-images/
+      await page.evaluate(async () => {
+        const selectors = Array.from(document.querySelectorAll("img"));
+        await Promise.all([
+          document.fonts.ready,
+          ...selectors.map((img) => {
+            // Image has already finished loading, let’s see if it worked
+            if (img.complete) {
+              // Image loaded and has presence
+              if (img.naturalHeight !== 0) return;
 
-            // If the image src is the same as the page href,
-            // don't wait for it to load, just pretend that it did
-            if (
-              img.src ===
-              document.location.origin + document.location.pathname
-            ) {
-              return img.dispatchEvent(new CustomEvent("load")); // eslint-disable-line
-            }
+              // If the image src is the same as the page href,
+              // don't wait for it to load, just pretend that it did
+              if (
+                img.src ===
+                document.location.origin + document.location.pathname
+              ) {
+                return img.dispatchEvent(new CustomEvent("load")); // eslint-disable-line
+              }
 
-            // Image failed, so it has no height
-            throw new Error(
-              `Image failed to load: ${img.src} on ${
+              // Image failed, so it has no height
+              throw new Error(
+                `Image failed to load: ${img.src} on ${
                 document.location.origin + document.location.pathname
               }`,
-            );
-          }
-          // Image hasn’t loaded yet, added an event listener to know when it does
-          return new Promise((resolve, reject) => {
-            img.addEventListener("load", resolve);
-            img.addEventListener("error", reject);
-          });
-        }),
-      ]);
-    });
+              );
+            }
+            // Image hasn’t loaded yet, added an event listener to know when it does
+            return new Promise((resolve, reject) => {
+              img.addEventListener("load", resolve);
+              img.addEventListener("error", reject);
+            });
+          }),
+        ]);
+      });
 
 
-    // Handle OpenAPI pages that can't be rendered as PDF
-    const isOpenApiPage = await page.evaluate(() => {
-      return !!document.querySelector("elements-api");
-    });
+      // Handle OpenAPI pages that can't be rendered as PDF
+      const isOpenApiPage = await page.evaluate(() => {
+        return !!document.querySelector("elements-api");
+      });
 
-    if (isOpenApiPage) {
-      console.log("==> Skipping OpenAPI Page");
-      await page.evaluate((url) => {
-        document.querySelector("body").innerHTML = `
+      if (isOpenApiPage) {
+        console.log("==> Skipping OpenAPI Page");
+        await page.evaluate((url) => {
+          document.querySelector("body").innerHTML = `
         <h1>${document.querySelector("title").innerText}</h1>
 
         <p>This content is not available in PDF format. Please see ${url.replace(
@@ -74,12 +76,12 @@ async function createPDF(name, urls) {
           "https://docs.konghq.com",
         )} for more details</p>
         `;
-      }, url);
-    }
+        }, url);
+      }
 
-    // Add PDF only styles
-    await page.addStyleTag({
-      content: `
+      // Add PDF only styles
+      await page.addStyleTag({
+        content: `
         @page { size: A4 portrait; margin: 24px; }
         pre { max-height: 100% !important; white-space: pre-wrap; }
         .code { padding-left: 0 !important; }
@@ -96,71 +98,75 @@ async function createPDF(name, urls) {
         .content td { display: table-cell !important; }
         .content th { display: table-cell !important; }
       `,
-    });
-
-    // Remove mobile css class from tables
-    await page.evaluate(() => {
-      Array.from(document.querySelectorAll("table.mobile")).forEach((t) => {
-        t.classList.remove("mobile");
       });
-    });
 
-    // Move header if we're on a plugin page
-    await page.evaluate(() => {
-      const header = document.querySelector(".page-header");
-      if (!header) {
-        return;
-      }
-      const content = document.querySelector(".page-content");
-      content.insertBefore(header, content.firstChild);
-    });
+      // Remove mobile css class from tables
+      await page.evaluate(() => {
+        Array.from(document.querySelectorAll("table.mobile")).forEach((t) => {
+          t.classList.remove("mobile");
+        });
+      });
 
-    // Convert tabs to be sequential with a header in each
-    await page.evaluate(() => {
-      const nodes = document.querySelectorAll("[data-navtab-id]");
-      for (const n of nodes) {
-        const id = n.attributes["data-navtab-id"].value;
-        const content = document.querySelector(
-          '[data-navtab-content="' + id + '"]',
-        );
-        n.innerHTML = "<h4>" + n.innerHTML + "</h4>";
-        content.parentNode.insertBefore(n, content);
-      }
-    });
+      // Move header if we're on a plugin page
+      await page.evaluate(() => {
+        const header = document.querySelector(".page-header--info");
+        if (!header) {
+          return;
+        }
+        const versionsDropdown = header.querySelector(".versions-dropdown");
+        versionsDropdown.parentNode.removeChild(versionsDropdown);
 
-    // Remove all un-needed parts of the page
-    await page.evaluate(() => {
-      const toRemove = [
-        ".content-header",
-        ".copy-action",
-        ".navtab-titles",
-        "header",
-        ".page--header-background",
-        ".page-header",
-        ".docs-sidebar",
-        ".docs-toc",
-        "footer",
-        ".modal",
-        ".feedback-widget-container",
-        "#scroll-to-top-button",
-        "#version-notice",
-        ".toggles"
-      ].join(", ");
-      const elements = document.querySelectorAll(toRemove);
-      for (let i = 0; i < elements.length; i++) {
-        elements[i].parentNode.removeChild(elements[i]);
-      }
-    });
+        const content = document.querySelector(".page-content-container");
+        content.insertBefore(header, content.firstChild);
+      });
 
-    // Remove all links
-    await page.evaluate(() => {
-      document
-        .querySelectorAll("a")
-        .forEach((n) => n.setAttribute("href", "#/"));
-    });
+      // Convert tabs to be sequential with a header in each
+      await page.evaluate(() => {
+        const nodes = document.querySelectorAll("[data-navtab-id]");
+        for (const n of nodes) {
+          const id = n.attributes["data-navtab-id"].value;
+          const content = document.querySelector(
+            '[data-navtab-content="' + id + '"]',
+          );
+          n.innerHTML = "<h4>" + n.innerHTML + "</h4>";
+          content.parentNode.insertBefore(n, content);
+        }
+      });
 
-    // Generate the PDF
-    merger.add(await page.pdf({ format: "A4", preferCSSPageSize: true }));
+      // Remove all un-needed parts of the page
+      await page.evaluate(() => {
+        const toRemove = [
+          ".content-header",
+          ".copy-action",
+          ".navtab-titles",
+          "header",
+          ".page--header-background",
+          ".page-header",
+          ".docs-sidebar",
+          ".docs-toc",
+          "footer",
+          ".modal",
+          ".feedback-widget-container",
+          "#scroll-to-top-button",
+          "#version-notice",
+          ".toggles"
+        ].join(", ");
+        const elements = document.querySelectorAll(toRemove);
+        for (let i = 0; i < elements.length; i++) {
+          elements[i].parentNode.removeChild(elements[i]);
+        }
+      });
+
+      // Remove all links
+      await page.evaluate(() => {
+        document
+          .querySelectorAll("a")
+          .forEach((n) => n.setAttribute("href", "#/"));
+      });
+
+      // Generate the PDF
+      merger.add(await page.pdf({ format: "A4", preferCSSPageSize: true }));
+    }
   }
 
   await browser.close();
