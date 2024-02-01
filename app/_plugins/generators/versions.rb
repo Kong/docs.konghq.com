860dd09a18d3f64167749a277e8420fe30d20e44
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2024-02-01 15:30:44 +0100

    Set Product data to `site` in a generator with the highest priority
    Lower the rest of the generators' priorities and update the code to use
    the cached data

--- app/_plugins/generators/versions.rb
+++ app/_plugins/generators/versions.rb
@@ -5,39 +5,7 @@ module Jekyll
   class Versions < Jekyll::Generator
     priority :high
 
-    def generate(site) # rubocop:disable Metrics/PerceivedComplexity, Metrics/MethodLength, Metrics/CyclomaticComplexity, Metrics/AbcSize
-      deck = Jekyll::GeneratorSingleSource::Product::Edition.new(edition: 'deck', site:)
-      mesh = Jekyll::GeneratorSingleSource::Product::Edition.new(edition: 'mesh', site:)
-      kic = Jekyll::GeneratorSingleSource::Product::Edition.new(edition: 'kubernetes-ingress-controller', site:)
-      gateway = Jekyll::GeneratorSingleSource::Product::Edition.new(edition: 'gateway', site:)
-      gateway_operator = Jekyll::GeneratorSingleSource::Product::Edition.new(edition: 'gateway-operator', site:)
-      konnect = Jekyll::GeneratorSingleSource::Product::Edition.new(edition: 'konnect', site:)
-      contributing = Jekyll::GeneratorSingleSource::Product::Edition.new(edition: 'contributing', site:)
-
-      site.data['editions'] = {}
-      site.data['editions']['deck'] = deck
-      site.data['editions']['mesh'] = mesh
-      site.data['editions']['kubernetes-ingress-controller'] = kic
-      site.data['editions']['gateway'] = gateway
-      site.data['editions']['gateway-operator'] = gateway_operator
-      site.data['editions']['konnect'] = konnect
-      site.data['editions']['contributing'] = contributing
-
-      site.data['kong_versions_deck'] = deck.releases.map(&:to_h)
-      site.data['kong_versions_mesh'] = mesh.releases.map(&:to_h)
-      site.data['kong_versions_konnect'] = konnect.releases.map(&:to_h)
-      site.data['kong_versions_kic'] = kic.releases.map(&:to_h)
-      site.data['kong_versions_contributing'] = contributing.releases.map(&:to_h)
-      site.data['kong_versions_gateway'] = gateway.releases.map(&:to_h)
-      site.data['kong_versions_kgo'] = gateway_operator.releases.map(&:to_h)
-
-      # Retrieve the latest version and put it in `site.data.kong_latest.version`
-      site.data['kong_latest_mesh'] = mesh.latest_release.to_h
-      site.data['kong_latest_KIC'] = kic.latest_release.to_h
-      site.data['kong_latest_deck'] = deck.latest_release.to_h
-      site.data['kong_latest_gateway'] = gateway.latest_release.to_h
-      site.data['kong_latest_kgo'] = gateway_operator.latest_release.to_h
-
+    def generate(site)
       # Add a `version` property to every versioned page
       # Also create aliases under /latest/ for all x.x.x doc pages
       site.pages.each do |page|
