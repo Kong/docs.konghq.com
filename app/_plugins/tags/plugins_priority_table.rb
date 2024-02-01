8c320dad8954d36c4caf2f8290462731f75ab356
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2024-02-01 10:43:35 +0100

    Render priorities table for all plugins No special case for correlation-id

--- app/_plugins/tags/plugins_priority_table.rb
+++ app/_plugins/tags/plugins_priority_table.rb
@@ -21,23 +21,12 @@ module Jekyll
         .parse(template)
         .render(
           { 'page' => @page,
-            'include' => { 'plugins' => plugins, 'type' => @type, 'correlation_id' => correlation_id } },
+            'include' => { 'plugins' => plugins, 'type' => @type } },
           { registers: context.registers }
         )
     end
 
     private
-
-    def correlation_id
-      if type == 'oss'
-        oss_priority = plugins['correlation-id']
-        free_priority = JSON.parse(File.read("#{PRIORITIES_PATH}/ee/#{version}.json"))['correlation-id']
-      else
-        free_priority = plugins['correlation-id']
-        oss_priority = JSON.parse(File.read("#{PRIORITIES_PATH}/oss/#{version}.json"))['correlation-id']
-      end
-      { 'free' => free_priority, 'oss' => oss_priority }
-    end
 
     def plugins
       @plugins ||= JSON.parse(File.read("#{PRIORITIES_PATH}/#{type}/#{version}.json"))
