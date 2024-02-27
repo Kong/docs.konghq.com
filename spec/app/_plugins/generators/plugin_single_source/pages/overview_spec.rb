ceac79a75ee25a2ceedc8d9d36e0572a36858ac1
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2024-02-27 11:40:47 +0100

    [DOCU-3662]: Improve plugin versioning by adding minimum_version and maximum_version support to frontmatters
    Before this change, whenever we added a new page for a specific version
    we had to copy the entire's plugin directory and move it into a new
    folder.
    
    This change adds support for `minimum_version` and `maximum_version` in
    the frontmatter of plugin pages.
    
    * If a page has one of these set in frontmatter, it gets generated
    for that version range, similar to versions.yml
    * If a page doesn’t have any minimum or maximum version,
    it defaults to the setting for the whole plugin, which is set in versions.yml
    
    The only caveat is that we had to replace `_metadata.yml` with a specific
    `_metadata` folder. If a file for a specific version exists, (e.g.
    `_metadata/_2.6.x.yml`) it will be used for that specific release, if
    not `_metadata/_index.yml` will be used.

--- spec/app/_plugins/generators/plugin_single_source/pages/overview_spec.rb
+++ spec/app/_plugins/generators/plugin_single_source/pages/overview_spec.rb
@@ -1,33 +1,20 @@
 RSpec.describe PluginSingleSource::Pages::Overview do
   let(:file) { 'overview/_index.md' }
   let(:plugin) { PluginSingleSource::Plugin::Base.make_for(dir: 'kong-inc/jwt-signer', site:) }
-  let(:release) { PluginSingleSource::Plugin::Release.new(site:, version:, plugin:, is_latest:, source:) }
+  let(:release) { PluginSingleSource::Plugin::Release.new(site:, version:, plugin:, is_latest:) }
 
   subject { described_class.new(release:, file:, source_path:) }
 
   describe '#content' do
+    let(:source_path) { File.expand_path('_hub/kong-inc/jwt-signer/', site.source) }
     let(:index_file_path) { File.expand_path('overview/_index.md', source_path) }
     let(:index) { markdown_content(File.expand_path(index_file_path, site.source)) }
+    let(:version) { '2.8.x' }
+    let(:is_latest) { true }
 
-    shared_examples_for 'returns the content of _index.md' do
-      it { expect(subject.content).to include(index) }
-    end
 
-    context 'when there is a specific folder for the version' do
-      let(:is_latest) { false }
-      let(:version) { '2.6.x' }
-      let(:source) { '_2.6.x' }
-      let(:source_path) { File.expand_path("_hub/kong-inc/jwt-signer/#{source}/", site.source) }
-
-      it_behaves_like 'returns the content of _index.md'
-    end
-
-    context 'when using `_index.md`' do
-      let(:is_latest) { true }
-      let(:source) { '_index' }
-      let(:version) { '2.8.x' }
-      let(:source_path) { File.expand_path('_hub/kong-inc/jwt-signer/', site.source) }
-      it_behaves_like 'returns the content of _index.md'
+    it 'returns the content of _index.md' do
+      expect(subject.content).to include(index)
     end
   end
 
@@ -39,7 +26,6 @@ RSpec.describe PluginSingleSource::Pages::Overview do
     context 'when it is the latest version of the plugin' do
       let(:is_latest) { true }
       let(:version) { '2.8.x' }
-      let(:source) { '_index' }
       let(:source_path) { File.expand_path('_hub/kong-inc/jwt-signer/', site.source) }
 
       it 'returns a hash containing the data needed to render the templates' do
@@ -70,13 +56,12 @@ RSpec.describe PluginSingleSource::Pages::Overview do
     context 'when it is not the latest version of the plugin' do
       let(:is_latest) { false }
       let(:version) { '2.6.x' }
-      let(:source) { '_2.6.x' }
-      let(:source_path) { File.expand_path("_hub/kong-inc/jwt-signer/#{source}/", site.source) }
+      let(:source_path) { File.expand_path("_hub/kong-inc/jwt-signer/", site.source) }
 
       it 'returns a hash containing the data needed to render the templates' do
         expect(subject.data).to include({
           'canonical_url' => '/hub/kong-inc/jwt-signer/',
-          'source_file' => '_hub/kong-inc/jwt-signer/_2.6.x/overview/_index.md',
+          'source_file' => '_hub/kong-inc/jwt-signer/overview/_index.md',
           'permalink' => '/hub/kong-inc/jwt-signer/2.6.x/',
           'ssg_hub' => false,
           'title' => 'Kong JWT Signer'
@@ -87,7 +72,6 @@ RSpec.describe PluginSingleSource::Pages::Overview do
     context 'when the page has frontmatter' do
       let(:is_latest) { true }
       let(:version) { '2.8.x' }
-      let(:source) { '_index' }
       let(:source_path) { File.expand_path('_hub/kong-inc/jwt-signer/', site.source) }
 
       it 'includes makes the content available on the page' do
@@ -97,33 +81,18 @@ RSpec.describe PluginSingleSource::Pages::Overview do
   end
 
   describe '#source_file' do
-    context 'when there is a specific folder for the version' do
-      let(:is_latest) { false }
-      let(:version) { '2.5.x' }
-      let(:source) { '_2.2.x' }
-      let(:source_path) { File.expand_path("_hub/kong-inc/jwt-signer/#{source}/", site.source) }
+    let(:is_latest) { true }
+    let(:version) { '2.8.x' }
+    let(:source_path) { File.expand_path('_hub/kong-inc/jwt-signer/', site.source) }
 
-      it 'returns the relative path to the _index.md file inside the corresponding folder' do
-        expect(subject.source_file).to eq('_hub/kong-inc/jwt-signer/_2.2.x/overview/_index.md')
-      end
-    end
-
-    context 'when using `_index.md`' do
-      let(:is_latest) { true }
-      let(:version) { '2.8.x' }
-      let(:source) { '_index' }
-      let(:source_path) { File.expand_path('_hub/kong-inc/jwt-signer/', site.source) }
-
-      it 'returns the relative path to the top-level _index.md file' do
-        expect(subject.source_file).to eq('_hub/kong-inc/jwt-signer/overview/_index.md')
-      end
+    it 'returns the relative path to the top-level _index.md file' do
+      expect(subject.source_file).to eq('_hub/kong-inc/jwt-signer/overview/_index.md')
     end
   end
 
   describe '#breadcrumbs' do
     let(:is_latest) { true }
     let(:version) { '2.8.x' }
-    let(:source) { '_index' }
     let(:source_path) { File.expand_path('_hub/kong-inc/jwt-signer/', site.source) }
 
     it 'returns a hash containing the page\'s breadcrumbs' do
---
+++ spec/fixtures/app/_hub/kong-inc/jwt-signer/how-to/nested/_tutorial-with-min-and-max.md
@@ -0,0 +1,9 @@
+---
+title: Nested tutorial with Min and Max
+nav_title: Nested Tutorial Nav title with Min and Max
+minimum_version: 2.8.x
+maximum_version: 3.0.x
+---
+
+# Nested Tutorial
+
