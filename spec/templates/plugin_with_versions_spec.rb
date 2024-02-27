0fc6d33f1e9f2dc5a025c40d0dd7bfb9a5bb9fd0
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2024-02-27 11:40:47 +0100

    [DOCU-3662]: Improve plugin versioning by adding minimum_version and maximum_version support to frontmatters
    Before this change, whenever we added a new page for a specific version
    we had to copy the entire's plugin directory and move it into a new
    folder.
    
    This change adds support for `minimum_version` and `maximum_version` in
    the frontmatter of plugin pages.
    
    * If a page has one of these set in frontmatter, it gets generated
    for that version range, similar to versions.yml
    * If a page doesn’t have any minimum or maximum version,
    it defaults to the setting for the whole plugin, which is set in versions.yml
    
    The only caveat is that we had to replace `_metadata.yml` with a specific
    `_metadata` folder. If a file for a specific version exists, (e.g.
    `_metadata/_2.6.x.yml`) it will be used for that specific release, if
    not `_metadata/_index.yml` will be used.

--- spec/templates/plugin_with_versions_spec.rb
+++ spec/templates/plugin_with_versions_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe 'Plugin page with multiple versions' do
 
     it 'renders the content' do
       expect(html).to have_css('h2', text: '_2.6.x Description')
-      expect(html).to have_css('p', text: 'content: Verify and (re-)sign one or two tokens in a request')
+      expect(html).to have_css('p', text: 'Verify and (re-)sign one or two tokens in a request')
     end
 
     it 'renders metadata in the header' do
@@ -49,8 +49,9 @@ RSpec.describe 'Plugin page with multiple versions' do
       expect(how_tos).to have_css('.sidebar-item:nth-of-type(2)', text: 'Nested')
       expect(how_tos).to have_css('.sidebar-item:nth-of-type(3)', text: 'Manage key signing')
 
-      nested_how_to = how_tos.find('.sidebar-item:nth-of-type(2)')
-      expect(nested_how_to).to have_css('.sidebar-item', text: 'Nested Tutorial Nav title')
+      nested_how_tos = how_tos.find('.sidebar-item:nth-of-type(2)', text: /^Nested$/)
+      expect(nested_how_tos).to have_css('.sidebar-item', text: 'Nested Tutorial Nav title')
+      expect(nested_how_tos).to have_css('.sidebar-item', text: 'Nested Tutorial Nav title with Min and Max')
     end
 
     context 'plugins that are `paid` or `premium`' do
