6eceb757d403a64c4f748a023cd529f597af43fc
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2024-04-22 15:49:08 +0200

    Update the way we handle overwriting the content of a page that isn't single-sourced depending on whether the actual file is translated and the nav file

--- app/_plugins/generators/pages/static_page_missing_translation.rb
+++ app/_plugins/generators/pages/static_page_missing_translation.rb
@@ -1,39 +1,10 @@
 # frozen_string_literal: true
 
-require_relative 'productable'
+require_relative 'overwrites/base'
 
 module Jekyll
   module Pages
     class StaticPageMissingTranslation
-      include Jekyll::Pages::Productable
-
-      class OverridePageWithTranslatedContent
-        def initialize(site:, page:, i18n_file:)
-          @site = site
-          @page = page
-          @i18n_file = i18n_file
-          @data = {}
-        end
-
-        def process! # rubocop:disable Metrics/AbcSize
-          content = File.read(@i18n_file.full_file_path_in_locale)
-          if content =~ Jekyll::Document::YAML_FRONT_MATTER_REGEXP
-            content = Regexp.last_match.post_match
-            @data = SafeYAML.load(Regexp.last_match(1))
-          end
-
-          # The sidenav is set in NavItemsData and we don't check
-          # if the docs_nav file exists in the locale.
-          # Files in app/<product> are not single-sourced,
-          # so if they are translated, the corresponding docs_nav file
-          # must be translated too.
-          @page.content = content
-          @page.data.merge!(@data)
-          @page.data['locale'] = @site.config['locale']
-          @page.data['full_file_path_in_locale'] = @i18n_file.full_file_path_in_locale
-        end
-      end
-
       def initialize(site:, page:)
         @site = site
         @page = page
@@ -46,22 +17,11 @@ module Jekyll
         check_for_translated_version
       end
 
-      def check_for_translated_version # rubocop:disable Metrics/MethodLength
+      def check_for_translated_version
         return if @page.is_a?(Jekyll::GeneratorSingleSource::SingleSourcePage)
         return if @page.is_a?(PluginSingleSource::SingleSourcePage)
 
-        i18n_file = Jekyll::GeneratorSingleSource::I18nFile.new(
-          file: @page.path,
-          src_path: '',
-          locale: @site.config['locale'],
-          site: @site
-        )
-        if i18n_file.exists_in_locale?
-          OverridePageWithTranslatedContent
-            .new(site: @site, page: @page, i18n_file:).process!
-        else
-          @page.data.merge!(Jekyll::Pages::TranslationMissingData.new(@site).data)
-        end
+        Overwrites::Base.make_for(site: @site, page: @page).process!
       end
     end
   end
