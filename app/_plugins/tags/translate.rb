bf1ad66ac0636188491e161296ca673d494fca92
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2024-04-26 08:44:46 +0200

    Fix string interpolation

--- app/_plugins/tags/translate.rb
+++ app/_plugins/tags/translate.rb
@@ -5,11 +5,30 @@ module Jekyll
     def initialize(tag_name, markup, tokens)
       super
 
-      @key = markup.strip
+      @translation_key, *@params = markup.strip.split(/\s+/)
+      @params_hash = {}
+      @params.each do |param|
+        key, value = param.split('=')
+        @params_hash[key.strip.to_sym] = value.strip
+      end
     end
 
     def render(context)
-      Liquid::Template.parse(I18n.t(@key)).render(context)
+      locale = context.registers[:site].config['locale']
+
+      # Render the parameter values with the Liquid context
+      rendered_params = {}
+      @params_hash.each do |key, value|
+        rendered_value = if value.start_with?('"') && value.end_with?('"')
+                           value[1..-2] # Remove surrounding quotes from string values
+                         else
+                           Liquid::VariableLookup.new(value).evaluate(context)
+                         end
+        rendered_params[key] = rendered_value
+      end
+
+      # Render the translation with the rendered parameters
+      I18n.t(@translation_key, locale: locale, **rendered_params)
     end
   end
 end
