61466fe9b292fb749894de72b36524925af508a2
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2024-07-02 16:51:24 +0200

    Add sitemap index to the site and update Robots.txt accordingly

--- app/_plugins/generators/sitemap.rb
+++ app/_plugins/generators/sitemap.rb
@@ -1,15 +1,46 @@
 # frozen_string_literal: true
 
 module Sitemap
+  class Page < Jekyll::Page
+    def initialize(site, basename, domain) # rubocop:disable Lint/MissingSuper, Metrics/MethodLength
+      @site = site
+      @base = site.source
+      @dir = '/sitemap-index/'
+      @basename = basename
+      @ext = '.xml'
+
+      @content = ''
+
+      @data = {
+        'layout' => 'sitemap',
+        'permalink' => "#{@dir}#{@basename}#{@ext}",
+        'domain' => domain,
+        'no_version' => true
+      }
+    end
+  end
+
   class Generator < Jekyll::Generator
     priority :low
 
     def generate(site)
       return if ENV['JEKYLL_ENV'] == 'development'
+      return if site.config['locale'] != I18n.default_locale.to_s
 
-      index = SEO::Index.new(site)
+      @site = site
+
+      index = SEO::Index.new(@site)
       index.generate
-      site.data['sitemap_pages'] = SEO::Sitemap.generate(index)
+      @site.data['sitemap_pages'] = SEO::Sitemap.generate(index)
+
+      @site.pages.concat(generate_sitemaps)
+    end
+
+    def generate_sitemaps
+      [
+        ::Sitemap::Page.new(@site, 'default', @site.config.dig('links', 'locales', 'en')),
+        ::Sitemap::Page.new(@site, 'jp', @site.config.dig('links', 'locales', 'ja'))
+      ]
     end
   end
 end
