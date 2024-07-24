82e5be167787c9af91c170c8d803b88ffda66916
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2024-07-24 09:56:55 +0200

    Use locale + country code for the site's locale

--- app/_plugins/i18n.rb
+++ app/_plugins/i18n.rb
@@ -7,4 +7,4 @@ I18n.enforce_available_locales = false
 I18n::Backend::Simple.include I18n::Backend::Fallbacks
 I18n.load_path += Dir["#{File.expand_path('config/locales')}/**/*.yml"]
 I18n.fallbacks = ['en']
-I18n.default_locale = 'en'
+I18n.default_locale = 'en-US'
