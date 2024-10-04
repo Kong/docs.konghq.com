9efa62bdbc9f9799fe76bc0d6b82c0a486599a35
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2024-10-04 13:46:07 +0200

    Add missing generators

---
+++ app/_plugins/tags/referenceable_fields/table_tag.rb
@@ -0,0 +1,62 @@
+# frozen_string_literal: true
+
+require 'json'
+
+module Jekyll
+  class ReferenceableFieldsTable < Liquid::Tag
+    def render(context) # rubocop:disable Metrics/AbcSize
+      environment = context.environments.first
+
+      super
+
+      version = context.environments.first['page']['release']
+      hub = context.registers[:site].data['ssg_hub']
+
+      @referenceable_fields = ReferenceableFields.run(version:, hub:)
+
+      template_file = File.read(File.expand_path('table_template.erb', __dir__))
+      template = ERB.new(template_file)
+      template.result(binding)
+    end
+  end
+
+  class ReferenceableFields
+    FILES_PATH = 'app/_src/.repos/kong-plugins/data/referenceable_fields/'
+
+    def self.run(version:, hub:)
+      new(version:, hub:).run
+    end
+
+    def initialize(version:, hub:)
+      @version = version
+      @hub = hub
+    end
+
+    def run
+      referenceable_fields.each_with_object({}) do |(slug, fields), result|
+        page = plugin_page(slug)
+
+        next unless page
+
+        result[slug] = [page.data['name'], fields]
+      end
+    end
+
+    def referenceable_fields
+      @referenceable_fields ||= begin
+        file_path = File.join(FILES_PATH, "#{@version}.json")
+        if File.exist?(file_path)
+          JSON.parse(File.read(file_path))
+        else
+          {}
+        end
+      end
+    end
+
+    def plugin_page(slug)
+      @hub.detect { |plugin| plugin.data['extn_slug'] == slug }
+    end
+  end
+end
+
+Liquid::Template.register_tag('referenceable_fields_table', Jekyll::ReferenceableFieldsTable)
