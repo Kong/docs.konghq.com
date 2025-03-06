1e03a924d34a10d203287458f4a1711a9e25fc4c
kong-docs <116832528+kong-docs@users.noreply.github.com>
2025-03-06 18:32:52 -0300

    chore(deps): bump kumahq/kuma-website from 5cf80116 to 502e3f8f (#8516)
    * chore(deps): bump kumahq/kuma-website from 5cf80116 to 502e3f8f
    
    Signed-off-by: kong-docs[bot] <team-docs@konghq.com>
    
    * chore: sync changes in tabs plugin
    
    Signed-off-by: Bart Smykla <bartek@smykla.com>
    
    * chore: remove unnecessary templates.erb file
    
    Signed-off-by: Bart Smykla <bartek@smykla.com>
    
    * chore: adjust rubocop comments
    
    Signed-off-by: Bart Smykla <bartek@smykla.com>
    
    ---------
    
    Signed-off-by: kong-docs[bot] <team-docs@konghq.com>
    Signed-off-by: Bart Smykla <bartek@smykla.com>
    Co-authored-by: kong-docs[bot] <team-docs@konghq.com>
    Co-authored-by: Bart Smykla <bartek@smykla.com>

--- app/_plugins/kuma-specific/tabs/tabs.rb
+++ app/_plugins/kuma-specific/tabs/tabs.rb
@@ -6,17 +6,32 @@ require 'securerandom'
 module Jekyll
   module Tabs
     class TabsBlock < Liquid::Block
-      def initialize(tag_name, config, tokens)
+      def initialize(tag_name, config, tokens) # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
         super
 
         @name, options = config.split(' ', 2)
         @options = options.split.each_with_object({}) do |o, h|
           key, value = o.split('=')
-          h[key] = value
+          h[key] = value || ''
         end
+
+        # Ensure 'useUrlFragment':
+        # - Defaults to 'true' if not present
+        # - If present without a value, it's 'true'
+        # - If present with a value, it keeps that value
+        @options['useUrlFragment'] =
+          if @options.key?('useUrlFragment')
+            @options['useUrlFragment'].empty? ? 'true' : @options['useUrlFragment']
+          else
+            'true'
+          end
+
+        # Normalize 'additionalClasses' by stripping quotes and ensuring proper spacing
+        additional_classes = @options.fetch('additionalClasses', '').gsub(/^"|"$/, '').strip
+        @options['additionalClasses'] = additional_classes.empty? ? '' : " #{additional_classes}"
       end
 
-      def render(context) # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
+      def render(context) # rubocop:disable Metrics/AbcSize
         environment = context.environments.first
         environment['tabs'] ||= {}
         file_path = context.registers[:page]['dir']
@@ -30,10 +45,48 @@ module Jekyll
 
         super
 
-        options = @options
-        template_file = File.read(File.expand_path('template.erb', __dir__))
-        template = ERB.new(template_file)
-        template.result(binding)
+        ERB.new(self.class.template).result(binding)
+      end
+
+      def self.template
+        <<~ERB
+          <div
+            class="tabs-component navtabs<%= @options['additionalClasses'] %>"
+            data-tab="<%= SecureRandom.uuid %>"
+            data-use-url-fragment="<%= @options['useUrlFragment'] %>"
+          >
+            <div role="tablist" class="tabs-component-tabs navtab-titles">
+              <% environment['tabs'][file_path][@name].each_with_index do |(hash, value), index| %>
+                <div class="tabs-component-tab navtab-title<%= index == 0 ? ' active' : '' %>" role="presentation">
+                  <a
+                    aria-controls="<%= hash.rstrip.gsub(' ', '-') %>"
+                    aria-selected="<%= index == 0 %>"
+                    href="#<%= hash.rstrip.gsub(' ', '-') %>"
+                    class="tabs-component-tab-a"
+                    role="tab"
+                    data-slug="<%= hash.rstrip.gsub(' ', '-') %>"
+                  >
+                    <%= hash %>
+                  </a>
+                </div>
+              <% end %>
+            </div>
+
+            <div class="tabs-component-panels navtab-contents">
+              <% environment['tabs'][file_path][@name].each_with_index do |(key, value), index| %>
+                <section
+                  aria-hidden="<%= index != 0 %>"
+                  class="tabs-component-panel navtab-content<%= index != 0 ? ' hidden' : '' %>"
+                  id="<%= key.rstrip.gsub(' ', '-') %>"
+                  role="tabpanel"
+                  data-panel="<%= key.rstrip.gsub(' ', '-') %>"
+                >
+                  <%= value %>
+                </section>
+              <% end %>
+            </div>
+          </div>
+        ERB
       end
     end
 
