59558547936771c8916824e9754770e1c58c9212
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2025-05-12 10:36:28 +0200

    Make rubocop happy

--- app/_plugins/hooks/dev_site_link.rb
+++ app/_plugins/hooks/dev_site_link.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require'yaml'
+require 'yaml'
 
 REDIRECTS = YAML.load_file('./redirects.yaml')
 
@@ -8,11 +8,10 @@ def dev_site_url_for(page)
   dev_site_url = page.site.config.dig('links', 'dev_site')
   page_url = page.data['canonical_url'] || page.url
   entry = REDIRECTS.detect { |u| u['docs_url'] == page_url }
-  url = entry && entry['dev_site_url'] || '/'
+  url = (entry && entry['dev_site_url']) || '/'
 
   "#{dev_site_url}#{url}"
 end
-
 
 Jekyll::Hooks.register :site, :pre_render do |site|
   site.pages.each do |page|
